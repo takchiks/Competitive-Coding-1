adda288af16e1f0d1f4db9f614d4cb0b3f4c5b3c
takudzwa <takchiks@gmail.com>
2026-02-09 20:27:30 -0500

    [Missing Number problem]

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,18 @@
+class Problem1 {
+    int missingNumber(int[] arr) {
+        int left = 0;
+        int right = arr.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
 
+            if (arr[mid] == mid + 1) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        // left is the first index where arr[i] != i + 1
+        return left + 1;
+    }
+}
